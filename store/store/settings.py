c2bcc445d7108fc445ef0c2501769fa9571a4941
victor-aunon <hola@victoraunon.com>
2023-01-13 13:48:04 +0100

    Including django-debug-toolbar

--- store/store/settings.py
+++ store/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "debug_toolbar",
     "api"
 ]
 
@@ -50,9 +51,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = "store.urls"
+
+# Django toolbar
+INTERNAL_IPS = ["127.0.0.1"]
 
 TEMPLATES = [
     {
--- store/store/urls.py
+++ store/store/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from store.views import commands
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("commands/", commands, name="commands")
+    path("commands/", commands, name="commands"),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__", include(debug_toolbar.urls))] + urlpatterns
